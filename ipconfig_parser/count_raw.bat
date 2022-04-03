95731de1587d0b00e83fa7c5eee254355b031707
reggiERj7 <102916031+reggiERj7@users.noreply.github.com>
2022-04-03 19:31:30 +0300

    Delete ipconfig_parser directory

--- ipconfig_parser/count_raw.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-findstr "172.16" 006.txt >> E:\\count.txt
--- ipconfig_parser/ipconfig_v2.0.bat
+++
@@ -1,5 +0,0 @@
-chcp 65001
-@echo off
-ipconfig | findstr "IPv4" | findstr "172.16." >> F:\\006.txt
-getmac /FO list /V >> F:\\006.txt
-echo ------------------------------------------------------------------------- >>F:\\006.txt
